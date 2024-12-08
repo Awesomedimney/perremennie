2ee863ef54110be0830866763a3206a03edf2688
Awesomedimney <dimas7kom@gmail.com>
2024-12-08 23:10:13 +0300

    Add files via upload

---
+++ peremennie.py
@@ -0,0 +1,5 @@
+confirm_home_work =12
+time = 1.5
+name_curse = 'Python,'
+time_one_task = (time / confirm_home_work)
+print("Курс:",name_curse, "всего задач:", confirm_home_work,",затрачено часов:", time, ",среднее время выполнение",time_one_task, "часа")
